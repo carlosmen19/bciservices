dce30eb4e8f41cada772d5ad19a0e5df11cf956a
carlosmen19 <karogini3@gmail.com>
2025-07-04 17:59:14 -0500

    Delete index_files directory

--- index_files/autoptimize_single_fde5f9be3defe540a4191971980550aa.php
+++
@@ -1,1 +0,0 @@
-@media only screen and (-Webkit-min-device-pixel-ratio:1.5),only screen and (-moz-min-device-pixel-ratio:1.5),only screen and (-o-min-device-pixel-ratio:3/2),only screen and (min-device-pixel-ratio:1.5){.footer_inner .logo>img,.header_mid_inner .logo img{display:none}.footer_inner .logo>.footer_logo_retina,.header_mid_inner .logo .logo_retina{display:inline-block}}
